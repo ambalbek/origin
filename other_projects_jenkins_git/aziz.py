0df6e46ebec285743193ba9e6c0476d555b52a85
AVI8-R G <azal88@yahoo.com>
2020-03-17 23:50:16 -0500

    python aziz.py is updated

--- other_projects_jenkins_git/aziz.py
+++ other_projects_jenkins_git/aziz.py
@@ -1,1 +1,22 @@
-[['akmanai' for i in range(100) if i%35 ==0] for i in range(20) if i%3==0]
+def score(x):
+    if x>90:
+        return "A"
+    elif x<=90 or x>80:
+        return "B"
+    elif x<=80 or x>70:
+        return "C"
+    elif x<=70 or x>60:
+        return "D"
+    else:
+        return "F"
+
+
+def main():
+    grades = (43,78,90,37,99,57,89,68,83,85,88)
+    grades = sorted(grades)
+    letters = list(map(score,grades))
+    print(letters)
+
+
+if __name__=="__main__":
+    main()
